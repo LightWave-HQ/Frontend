14ab38f10184196f16e5c32834220b0acf434aae
Witty-Wizard <agarwalshashank429@gmail.com>
2024-12-03 17:28:08 +0530

    Added: Basic React Application

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { Box } from "@mui/material";
+
+function App() {
+  return <Box>Hello</Box>;
+}
+
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/js/index.js',
-        assetFileNames: (assetInfo) => {
-          if (/\.(css)$/.test(assetInfo.name)) {
-            return 'assets/css/index.css';
-          }
-          return 'assets/[name]-[hash].[ext]';
-        },
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
